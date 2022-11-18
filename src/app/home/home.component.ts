2713cd5987e30fbabe1169bcb239ff2c92cdeeb8
MasonT001 <mltrepagnier@gmail.com>
2022-11-18 11:15:42 -0800

    Getting closer to the core of observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { count } from 'console';
+
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private firstObsSubscription: Subscription
 
   constructor() { }
 
   ngOnInit() {
+     this.firstObsSubscription = interval(1000).subscribe( count => {
+       console.log(count)
+     })
   }
 
 }
